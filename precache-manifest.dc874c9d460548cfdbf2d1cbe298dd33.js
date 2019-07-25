20c8fa4f19abe1125715037eacc43bc6129b2a07
Szulby <slawek.sulbinski@gmail.com>
2019-07-25 15:40:28 +0200

    Updates

---
+++ precache-manifest.dc874c9d460548cfdbf2d1cbe298dd33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b911867c494f546196824dd28d90f97e",
+    "url": "/E-commerce-with-React/index.html"
+  },
+  {
+    "revision": "b49631d02f99a18b5aca",
+    "url": "/E-commerce-with-React/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d722c614cc0fc064f852",
+    "url": "/E-commerce-with-React/static/css/main.759dbddc.chunk.css"
+  },
+  {
+    "revision": "b49631d02f99a18b5aca",
+    "url": "/E-commerce-with-React/static/js/2.20fbab7e.chunk.js"
+  },
+  {
+    "revision": "d722c614cc0fc064f852",
+    "url": "/E-commerce-with-React/static/js/main.2f2d1f14.chunk.js"
+  },
+  {
+    "revision": "8a21ba26e2a952d057d1",
+    "url": "/E-commerce-with-React/static/js/runtime~main.5e94abec.js"
+  },
+  {
+    "revision": "f805fc8051bce5a8b31bda7cdaca1a40",
+    "url": "/E-commerce-with-React/static/media/jumbotron.f805fc80.png"
+  }
+]);
